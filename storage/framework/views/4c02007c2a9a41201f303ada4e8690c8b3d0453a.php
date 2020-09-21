056c1f050a69c60f5f715feaab7e451368557b03
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-09-21 13:44:12 +0200

    Laravel initial commit.

---
+++ storage/framework/views/4c02007c2a9a41201f303ada4e8690c8b3d0453a.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Book</title>
+</head>
+<body>
+<h1>Book</h1>
+
+<p><?php echo e($book->title); ?></p>
+
+</body>
+</html>
+<?php /**PATH /Users/robinvandoorn/code/redex/resources/views/book.blade.php ENDPATH**/ ?>
