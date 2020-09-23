afa959e4b8636a848fdd99602a649a94a043c4ee
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-09-23 17:20:40 +0200

    Update books controller.

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,15 +2,110 @@
 
 namespace App\Http\Controllers;
 
-use DB;
 use App\Models\Book;
-
 use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
-    public function show()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index()
     {
-       return view('book');
+        $books = Book::all();
+        return view('books.index', [
+            'books' => $books
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $books = new Book();
+        $books->title = $request->get('title');
+        $books->description = $request->get('description');
+        $books->image = $request->get('image');
+        $books->author = $request->get('author');
+
+        $books->save();
+        return redirect('books')->with('success', "Boek is opgeslagen!");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function show($id)
+    {
+        try {
+            $book = Book::find($id);
+            $error = null;
+        } catch (\Exception $e) {
+            $book = null;
+            $error = $e->getMessage();
+        }
+
+        return view('books.show', [
+            'book' => $book,
+            'error' => $error
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
