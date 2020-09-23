584ca37a1be21dd64435d4eaba6f137677e8edf9
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-09-23 17:42:12 +0200

    Edit index.blade.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <header class="jumbotron">
-        <h1 class="modal-title float-left">News</h1>
-        <a  class="nav-link float-right" href="{{route('news.create')}}">Maak een nieuw bericht aan.</a>
+        <h1 class="modal-title float-left">Books</h1>
+        <a  class="nav-link float-right" href="{{route('books.create')}}">Voeg een nieuw book toe.</a>
     </header>
 
     <div class="container">
@@ -13,12 +13,12 @@
             </div>
         @endif
         <div class="row">
-            @foreach($newsItems as $newsItem)
+            @foreach($books as $book)
                 <div class="col-sm card border-9">
-                    <h2 class="card-title">{{$newsItem['title']}}</h2>
-                    <p class="card-text">{{$newsItem['description']}}</p>
-                    <img src="{{$newsItem['image']}}" alt="{{$newsItem['title']}}" class="card-img">
-                    <a  class="btn btn-light" href="{{route('news.show', $newsItem['id'])}}">Lees meer</a>
+                    <h2 class="card-title">{{$book['title']}}</h2>
+                    <p class="card-text">{{$book['author']}}</p>
+                    <img src="{{$book['image']}}" alt="{{$book['title']}}" class="card-img">
+                    <a  class="btn btn-light" href="{{route('books.show', $book['id'])}}">Lees meer</a>
                 </div>
             @endforeach
         </div>
