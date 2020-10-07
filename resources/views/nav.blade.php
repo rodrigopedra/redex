3c1b4089f09ab6a704db2fef7bbd24abeec6bd3f
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-10-07 14:23:42 +0200

    Create nav.blade.php

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,6 @@
+<div class="links">
+    <a href="{{route('books')}}">Home</a>
+    <a href="{{route('books')}}">Search</a>
+    <a href="{{route('books')}}">Reading List</a>
+    <a href="{{route('books')}}">Profile</a>
+</div>
