f060464cd07a419e32a4903f6da317d5a22fd6d5
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-10-07 14:22:40 +0200

    npm run dev.

--- public/js/app.js
+++ public/js/app.js
@@ -49722,6 +49722,7 @@ module.exports = function(module) {
  */
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+yes;
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
 /**
  * The following block of code may be used to automatically register your
