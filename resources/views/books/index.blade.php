f74c4d7a7dc583d6a100e789c6aca34d9660984b
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-10-16 15:56:33 +0200

    Edit styling.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <header class="jumbotron">
-        <a  class="nav-link float-right add_link" href="{{route('books.create')}}">add new book</a>
-    </header>
 
     <div class="container">
         @if ($message = Session::get('success'))
@@ -11,15 +8,14 @@
                 <storng>{{ $message }}</storng>
             </div>
         @endif
-        <div class="row">
+        <div class="row flex m-6">
             @foreach($books as $book)
-                <div class="col-sm card border-9">
+                <div class="col-sm card border-9 m-6 text-center">
+                    <img src="{{$book['image']}}" alt="{{$book['title']}}" class="card-img">
                     <h2 class="card-title">{{$book['title']}}</h2>
                     <p class="card-text">{{$book['author']}}</p>
-                    <img src="{{$book['image']}}" alt="{{$book['title']}}" class="card-img">
                     <a  class="btn btn-light" href="{{route('books.show', $book['id'])}}">Lees meer</a>
                     <a  class="btn btn-light" href="#">Voeg toe aan leeslijst</a>
-
                 </div>
             @endforeach
         </div>
