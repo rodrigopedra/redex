96628dabac873dd425ad3410816e8dd5bc8b695e
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-10-23 18:09:41 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\CommentsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-//books
+Route::get('/', [BooksController::class, 'index'])->name('books');
 Route::get('/books', [BooksController::class, 'index'])->name('books');
 Route::get('books/create', [BooksController::class, 'create'])->name('books.create')->middleware('auth');
 Route::post('books/store', [BooksController::class, 'store'])->name('books.store');
 Route::get('books/{id}', [BooksController::class, 'show'])->name('books.show');
 
-Route::post('/books/{book}/comments', [\App\Http\Controllers\CommentsController::class, 'store']);
-
-
-
+Route::post('/books/{book}/comments', [CommentsController::class, 'store'])->name('comments.store');
 
 Auth::routes();
 
