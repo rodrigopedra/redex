b8600e7eb848a9ad53781b77611596cb5f58a86f
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-10-29 17:17:20 +0100

    Add resources route.

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\CommentsController;
 
+Auth::routes();
 
 Route::get('/', [BooksController::class, 'index'])->name('books');
-Route::get('/books', [BooksController::class, 'index'])->name('books');
-Route::get('books/create', [BooksController::class, 'create'])->name('books.create')->middleware('auth','admin');
-Route::post('books/store', [BooksController::class, 'store'])->name('books.store');
-Route::get('books/{id}', [BooksController::class, 'show'])->name('books.show');
+
+Route::resources(['books' => BooksController::class,]);
 
 Route::post('/books/{book}/comments', [CommentsController::class, 'store'])->name('comments.store');
-
-Auth::routes();
 
 Route::get('/home', [BooksController::class, 'index'])->name('home');
