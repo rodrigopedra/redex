1056cc9b7fc24e2aeb0e0baa141920c51e4d765e
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-10-29 17:16:50 +0100

    Delete nav.blade.php

--- resources/views/nav.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="links">
-    <a href="{{route('books')}}">Home</a>
-    <a href="{{route('books')}}">Search</a>
-    <a href="{{route('books')}}">Reading List</a>
-    <a href="{{route('books')}}">Profile</a>
-</div>
